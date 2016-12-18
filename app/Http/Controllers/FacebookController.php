9b6988b808c51ecc1655ab41ff183851640286ad
Ayeni Olusegun <nsegun5@gmail.com>
2016-12-18 22:34:12 +0100

    fix bugs

--- app/Http/Controllers/FacebookController.php
+++ app/Http/Controllers/FacebookController.php
@@ -11,17 +11,28 @@ use App\Http\Requests;
 
 class FacebookController extends Controller{
     public function index(Request $request){
+        $this->deleteFacebookToken();
         $tokenData = $this->getFacebookToken();
         if(!$tokenData){
             return redirect('auth/facebook?redirect=' . $request->path());
         } else {
-            $dataFromFacebook = json_decode($this->getData($tokenData)->getGraphUser(), true);
+            try {
+                $dataFromFacebook = json_decode($this->getData($tokenData)->getGraphUser(), true);
+            } catch (Exception $e) {
+                $this->deleteFacebookToken();
+                return redirect('auth/facebook?redirect=' . $request->path());
+            }
             return view('api.facebook')->with('details', $dataFromFacebook);
         }
+
     }
 
     private function getFacebookToken(){
         return UserProfile::whereProvider('facebook')->where('user_id', '=', Auth::user()->id)->first();
+    }
+
+    private function deleteFacebookToken(){
+        return UserProfile::whereProvider('facebook')->where('user_id', '=', Auth::user()->id)->delete();
     }
 
     private function getData($data){
